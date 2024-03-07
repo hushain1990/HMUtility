33e91fdce00bdbe27746682dd403ff080dfea7bf
HMK <hushain1990@gmail.com>
2024-03-07 13:42:40 +0530

    Code Commit

--- Example/HMUtility/ViewController.swift
+++ Example/HMUtility/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import HMUtility
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("\(Utility.isValidEmail("hushain1990@gmail.com"))")
     }
 
     override func didReceiveMemoryWarning() {
